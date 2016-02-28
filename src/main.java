7d59ebfc82540d841d357cdaced0e111f16389b6
danwardvs <danwardvs@adsgames.net>
2016-02-28 17:29:30 -0500

    Working fonts and background image

--- src/main.java
+++ src/main.java
@@ -16,6 +16,9 @@ import org.newdawn.slick.util.ResourceLoader;
 @SuppressWarnings("deprecation")
 public class main {
 	
+	int width = 800;
+	int height = 600;
+	
 	 /** The fonts to draw to the screen */
     private TrueTypeFont font;
     private TrueTypeFont font2;
@@ -42,7 +45,16 @@ public class main {
         font = new TrueTypeFont(awtFont, antiAlias);
 		
 		 // load font from file
-       
+        try {
+            InputStream inputStream = ResourceLoader.getResourceAsStream("OpenSans-Regular.ttf");
+             
+            Font awtFont2 = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+            awtFont2 = awtFont2.deriveFont(24f); // set font size
+            font2 = new TrueTypeFont(awtFont2, antiAlias);
+             
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 	
 	 public void updateFPS() {
@@ -96,8 +108,9 @@ public class main {
 	    @SuppressWarnings("deprecation")
 		public void start() {
 	    	
+	    	
 	        try {
-	        Display.setDisplayMode(new DisplayMode(900,500));
+	        Display.setDisplayMode(new DisplayMode(width,height));
 	        Display.create();
 	    } catch (LWJGLException e) {
 	        e.printStackTrace();
@@ -106,13 +119,27 @@ public class main {
 	        
 	        lastFPS = getTime();
 
+
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
+			GL11.glShadeModel(GL11.GL_SMOOTH);        
+			GL11.glDisable(GL11.GL_DEPTH_TEST);
+			GL11.glDisable(GL11.GL_LIGHTING);                    
+	 
+			GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);                
+	        GL11.glClearDepth(1);                                       
+	 
+	        GL11.glEnable(GL11.GL_BLEND);
+	        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+	 
+	        GL11.glViewport(0,0,width,height);
+			GL11.glMatrixMode(GL11.GL_MODELVIEW);
+			 
+			GL11.glMatrixMode(GL11.GL_PROJECTION);
+			GL11.glLoadIdentity();
+			GL11.glOrtho(0, width, height, 0, 1, -1);
+			GL11.glMatrixMode(GL11.GL_MODELVIEW);
 	        
-	        // init OpenGL
-	        GL11.glMatrixMode(GL11.GL_PROJECTION);
-	        GL11.glLoadIdentity();
-	        GL11.glOrtho(0, 900, 0, 500, 1, -1);
-	        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-	        
+	        loadFont();
 	        
 	        while (!Display.isCloseRequested()) {
 	        	int delta = getDelta();
@@ -120,8 +147,9 @@ public class main {
 	            // Clear the screen and depth buffer
 	            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);  
 	            
-	            Color.white.bind();
-	            //font.drawString(100, 100, "NICE LOOKING FONTS!", Color.yellow);
+	        
+	            font2.drawString(25,25, "Welcome to Danny's Assorted Goods Market!", Color.green);
+
 
 	            update(delta);
 	            
